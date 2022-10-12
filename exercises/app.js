809c4d39458f87319178dc2b3fac189712971ded
Danielle <dsg1457.dg@gmail.com>
2022-10-12 15:58:31 -0600

    added html biolerplate,

---
+++ exercises/app.js
@@ -0,0 +1,18 @@
+function test(num) {
+    if(num > 0) {
+        console.log("Positive!");
+    } else if(num < 0) {
+        console.org("Negative!");
+    } else if (num === 0) {
+        console.log("Your answer is zero")
+
+    } else {
+        console.log("That is not a number")
+    }
+}
+
+//calling a function
+test(-5); //negative
+test(10) //calling functions again
+test(0)
+test("Danielle")
